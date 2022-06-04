90021def188ad29e332fa98a64c4e6912cdc1c47
Rory GAO <roryblucky@gmail.com>
2022-06-04 16:37:40 +0800

    Initial commit

---
+++ src/main/java/com/unitests/spring/UnitTestWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.unitests.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitTestWithSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnitTestWithSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unitests/spring/UnitTestWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unitests.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestWithSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
